e537e30acfd01c01765f7de2a9dd2a390b2ff253
Pierre <pierrefraisse90@gmail.com>
2023-05-26 17:02:31 +0200

    feat : UI

---
+++ app.js
@@ -0,0 +1,16 @@
+// select button
+// select inputs
+// select spans
+
+// get values of inputs
+
+// put values in span when button is clicked
+// animate values from 0 to age
+
+// Make sure day is a number between 1 and 28-29 in february, 30 31 days
+// Make sure month is between 1 and 12
+// make sure year is valid
+// make sure date is in the future
+
+// display error message for wrong value format
+// display error for wrong date
