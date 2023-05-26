7e2853e0580fa4d5a636a3a78298cdaf710458c5
Pierre <pierrefraisse90@gmail.com>
2023-05-26 18:11:55 +0200

    feat : getValues function, not yet finished

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 // select button
+const Btn = document.querySelector(".btn");
 // select inputs
+const inputs = document.querySelectorAll("input");
 // select spans
+const valueUpdates = document.querySelectorAll("span");
+// select paragraphs
+// const errorMessages = document.querySelectorAll(".alert");
 
 // get values of inputs
+const getValues = () => {
+  const valuesObject = Array.from(inputs).reduce(
+    (acc, input) => ({
+      ...acc,
+      [input.id]: input.value,
+    }),
+    {}
+  );
+  if (Object.keys(valuesObject).length !== inputs.length) {
+    inputs.forEach((input) => {
+      if (input.value === "") {
+        console.log("Field cannot be empty");
+        input.nextElementSibling.textContent = "Field cannot be empty";
+      }
+    });
+  }
+  return valuesObject;
+};
+
+Btn.addEventListener("click", getValues);
 
 // put values in span when button is clicked
+// reset value in input field
 // animate values from 0 to age
 
 // Make sure day is a number between 1 and 28-29 in february, 30 31 days
